ef25d9666f94f54ccef2c83caa6db2d0814d738e
Eliezer Martins <eliezer@gwmnet.com.br>
2021-07-31 00:52:32 -0300

    App Python v1.0

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def pagina_inicial():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+-- INSERT --                                                                                  2,1           All
+
